07e39404b727d2cfdb8ad703b4811ce2afaa5fef
axel-paillaud <contact@axelweb.fr>
2023-09-04 16:01:49 +0200

    fetch function to mock data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup>
+  import { ref } from 'vue';
   import Plant from '@/components/Plant.vue';
+
+  //Does I need a reactive variable here ?
+  let plantList = [];
+
+  fetch('https://my-json-server.typicode.com/axel-paillaud/la-maison-jungle-vuejs/plants')
+  .then((response) => {
+    return response.json()
+    .then(data => {
+      plantList = data;
+      console.log(plantList);
+    })
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 </script>
 
 <template>
-  <main>
+  <main class="main">
     <div class="product-container">
       <Plant />
     </div>
@@ -12,7 +28,7 @@
 </template>
 
 <style scoped>
-  .product-container {
+  .main {
     display: flex;
     justify-content: center;
     padding: 64px;
